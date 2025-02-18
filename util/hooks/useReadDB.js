a03ec02360c2c9da6389785f7065f58b7fc7e811
secondarctic <second-arctic@outlook.com>
2025-02-17 19:07:16 -0500

    update components with custom hooks

--- util/hooks/useReadDB.js
+++ util/hooks/useReadDB.js
@@ -1,14 +1,34 @@
 import { useEffect, useState } from 'react';
-import { callDatabase } from '../appwrite/callDatabase';
+import { Client, Databases } from 'appwrite';
+import { callDatabase } from '../appwrite/callDatabase.js';
+import { appwriteAPI } from '../../lib/appwriteAPI.js';
+
+const projectID = import.meta.env.VITE_PROJECT_ID;
+const databaseID = import.meta.env.VITE_DATABASE_ID;
+const collectionID = import.meta.env.VITE_COLLECTION_ID;
 
 const useReadDB = () => {
 	const [characterInfo, setCharacterInfo] = useState([]);
 
+	const client = new Client();
+	client.setProject(projectID);
+
+	const databases = new Databases(client);
+
 	useEffect(() => {
-		const data = callDatabase();
-		const documents = data.documents;
-		setCharacterInfo(documents);
-	});
+		let result = databases.listDocuments(
+			databaseID,
+			collectionID,
+			[] // queries (optional)
+		);
+
+		result.then((res) => {
+			setCharacterInfo(res.documents);
+		}),
+			(err) => {
+				console.log(`error:${err}, id:${err.id}`);
+			};
+	}, []);
 
 	return [characterInfo];
 };
